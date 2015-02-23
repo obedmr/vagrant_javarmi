a2d3b56b884487571b4fb617a9cf09b2fce0c6e8
Obed N Munoz <obed.n.munoz@gmail.com>
2015-02-22 22:39:07 -0600

    Setting Source Code updater
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- scripts/server.sh
+++ scripts/server.sh
@@ -8,14 +8,21 @@ pacman -S --noconfirm jdk8-openjdk
 
 # Clone Java RMI Project
 cd /home/vagrant
-git clone https://github.com/obedmr/distributed_systems.git
-cd distributed_systems/src
+git clone https://github.com/VictorRodriguez/distributed_systems.git
+chown vagrant.vagrant -R distributed_systems
+#cd distributed_systems/src
+
+# Set Source Code Updater
+cp /vagrant/scripts/worker-update.* /usr/lib/systemd/system/
+systemctl daemon-reload
+systemctl enable worker-update.timer
+systemctl start worker-update.timer
 
 # Compile Server
-javac -d ./ example/hello/*.java
+#javac -d ./ example/hello/*.java
 
 # Starting RMI Registry
-rmiregistry &
+#rmiregistry &
 
 # Starting Server
-java -cp ./ -Djava.rmi.server.codebase=file:./ -Djava.rmi.server.hostname=10.0.0.10 example.hello.Server &
+#java -cp ./ -Djava.rmi.server.codebase=file:./ -Djava.rmi.server.hostname=10.0.0.10 example.hello.Server &
